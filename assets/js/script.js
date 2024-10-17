a20e7dfd0c1d7e15f76abdb6c2d5e47e8cc6bb80
Jonathan Coton <fbp3sgxt@students.codeinstitute.net>
2024-10-17 15:04:31 +0000

    ad user qol features

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -24,16 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
-
-    // if (gameType === "addition") {
-    //     displayAdditionQuestion(num1, num2);
-    // } else {
-    //     alert(`Unknown game type: ${gameType}`);
-    //     throw `Unknown game type: ${gameType}. Aborting!`;
-    // }
     
     switch (gameType) {
         case 'addition':
