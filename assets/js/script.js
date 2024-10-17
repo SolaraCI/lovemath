5215ba521b50b4856f75eac644d0856540ac929e
Jonathan Coton <fbp3sgxt@students.codeinstitute.net>
2024-10-17 14:52:53 +0000

    implement switch statement to display questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,11 +28,28 @@ function runGame(gameType) {
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
 
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
+    // if (gameType === "addition") {
+    //     displayAdditionQuestion(num1, num2);
+    // } else {
+    //     alert(`Unknown game type: ${gameType}`);
+    //     throw `Unknown game type: ${gameType}. Aborting!`;
+    // }
+    
+    switch (gameType) {
+        case 'addition':
+            displayAdditionQuestion(num1, num2);
+            break;
+        case 'subtract':
+            displaySubtractQuestion(num1, num2);
+            break;
+        case 'multiply':
+            displayMultiplyQuestion(num1, num2);
+            break;
+        // case '÷':
+        //     return [operand1 / operand2, "division"];
+        default:
+            alert(`Unknown game type: ${gameType}`);
+            throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
 
@@ -110,12 +127,20 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '-';
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '×';
+}
 
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '÷';
 }
